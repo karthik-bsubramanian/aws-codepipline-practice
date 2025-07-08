37f0372f12f9f8ac14469603122d0199051156f7
Karthik Balasubramanian <karthikbalasubramanian08@gmail.com>
2025-07-08 15:17:49 +0530

    init

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path'); // Required for path manipulation
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+// Define the directory where your static HTML files are located
+// app.use(express.static(path.join(__dirname, 'public'))); 
+
+// Route to serve a specific HTML file
+app.get('/yaqub', (req, res) => {
+  app.use(express.static(path.join(__dirname, 'public'))); 
+  res.sendFile(path.join(__dirname, 'public','index.html')); 
+});
+
+app.get('/', (req, res) => {
+  res.write("Application is deployed using codePipeline and this is the first version cheers!");
+  res.end();
+});
+app.listen(port)
+// app.listen(port, () => {
+//   console.log(`Server running at http://localhost:${port}`);
+// });
+
+
